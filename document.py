287b78673ea8d412790c1e5af6b5694f412d95cf
Rain <Rainyan@users.noreply.github.com>
2023-10-29 20:40:59 +0200

    wip

--- document.py
+++ document.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
+"""Automatic Markdown documentation for SourceMod plugin ConVars."""
+
+import argparse
 import os
+import re
 import sys
 from ast import literal_eval
 
@@ -10,10 +14,10 @@ import marko
 from marko.md_renderer import MarkdownRenderer
 
 
-DOC_HEADER_PATTERNS: list[str] = []
+VERSION = "0.1.0"
 
 
-def update_readme(cvars: list[sp_cvars.Cvar], input: str) -> str:
+def update_readme(cvars: list[sp_cvars.Cvar], input: str, header_patterns) -> str:
     if len(cvars) == 0:
         return input
 
@@ -29,12 +33,17 @@ def update_readme(cvars: list[sp_cvars.Cvar], input: str) -> str:
             if not isinstance(child, marko.block.Heading):
                 continue
             text = md.renderer.render_children(child)
-            if any((text.lower() == p.lower() for p in DOC_HEADER_PATTERNS)):
+            if header_patterns.fullmatch(text):
                 target = child  # Find target header
                 next_child = next(it)
                 # TODO: other types
-                if not any(isinstance(next_child, a) for a in (marko.block.List,)):
-                    print(f"Skip {type(next_child)}")
+                if not any(
+                    isinstance(next_child, a)
+                    for a in (
+                        marko.block.List,
+                        marko.block.Paragraph,
+                    )
+                ):
                     continue
                 # Remove old content
                 for _ in range(len(next_child.children)):  # type: ignore
@@ -74,32 +83,78 @@ def update_readme(cvars: list[sp_cvars.Cvar], input: str) -> str:
     return md.render(doc)
 
 
+def main():
+    parser = argparse.ArgumentParser(
+        prog=sys.argv[0],
+        description=__doc__,
+        epilog=f"Version {VERSION}",
+    )
+    parser.add_argument(
+        "-C",
+        "--code_patterns",
+        help="RegEx pattern for code files to match.",
+        default=r"^.*\.(sp|inc|SP|INC)$",
+    )
+    parser.add_argument(
+        "-D",
+        "--doc_patterns",
+        help="RegEx pattern for documentation files to match.",
+        default=r"^.*\.(md|MD)$",
+    )
+    parser.add_argument(
+        "-H",
+        "--header_patterns",
+        help="RegEx pattern for documentation headers to match for the location of the cvar docs placeholder.",
+        default=r"([Cc]vars|[Cc]on[Vv]ars|[Cc]onsole [Vv]ariables)",
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="If set, print the output instead of writing to file.",
+    )
+    parser.add_argument(
+        "--encoding",
+        help="Encoding to use for file read/write operations.",
+        default="utf-8",
+    )
+    args = parser.parse_args()
+
+    path_code = None
+    pattern_code = re.compile(args.code_patterns)
+    for root, _, files in os.walk(os.path.dirname(os.path.realpath(__file__))):
+        for file in files:
+            if pattern_code.match(file):
+                path_code = os.path.join(root, file)
+                break
+    assert path_code is not None
+    assert os.path.isfile(path_code)
+
+    path_doc = None
+    pattern_doc = re.compile(args.doc_patterns)
+    for root, subdirs, files in os.walk(os.path.dirname(os.path.realpath(__file__))):
+        for file in files:
+            if pattern_doc.match(file):
+                path_doc = os.path.join(root, file)
+    assert path_doc is not None
+    assert os.path.isfile(path_doc)
+
+    cvars = sp_cvars.parse_cvars(path_code)
+    with open(path_doc, mode="r", encoding=args.encoding) as f:
+        doc_input = f.read()
+    doc_output = update_readme(cvars, doc_input, re.compile(args.header_patterns))
+
+    if args.dry_run:
+        print(f"Input differs from output: {doc_input != doc_output}")
+        print(f"Length of output: {len(doc_output)}")
+        print("= = = OUTPUT STARTS = = =")
+        print(doc_output)
+        print("= = = OUTPUT ENDS = = =")
+        return
+
+    if doc_input != doc_output:
+        with open(path_doc, mode="w", encoding=args.encoding) as f:
+            f.write(doc_output)
+
+
 if __name__ == "__main__":
-    ENCODING = "utf-8"
-    DRY_RUN = False
-    try:  # TODO: argparse
-        PATH_CODE = sys.argv[1]
-        PATH_DOC = sys.argv[2]
-        DOC_HEADER_PATTERNS = [a.strip() for a in sys.argv[3].split(",")]
-        if len(sys.argv) > 4:
-            DRY_RUN = sys.argv[4] != "0"
-    except IndexError:
-        print(
-            f'Usage: {sys.argv[0]} "<code file>" "<doc file>" "<comma-delimited list of doc header patterns>"'
-        )
-        print(f"Example: python {sys.argv[0]} plugin.sp README.md cvars")
-        sys.exit(1)
-    assert len(DOC_HEADER_PATTERNS) > 0
-    for p in DOC_HEADER_PATTERNS:
-        assert len(p) > 0
-
-    CVARS = sp_cvars.parse_cvars(PATH_CODE)
-    with open(PATH_DOC, mode="r", encoding=ENCODING) as f:
-        DOC_INPUT = f.read()
-    DOC_OUTPUT = update_readme(CVARS, DOC_INPUT)
-
-    if DRY_RUN:
-        print(DOC_OUTPUT)
-    elif DOC_INPUT != DOC_OUTPUT:
-        with open(PATH_DOC, mode="w", encoding=ENCODING) as f:
-            f.write(DOC_OUTPUT)
+    main()
